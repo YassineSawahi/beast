6f36122e65807ca2e97a97c967a002e757d404e7
Melki Amen Allah <amenmelki28@gmail.com>
2025-01-26 19:58:49 +0100

    fixed more stuff

--- src/main.js
+++ src/main.js
@@ -3,12 +3,12 @@ import './ui/assets/main.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import PrimeVue from 'primevue/config'
-import Lara from '@primevue/themes/lara'
+import Aura from '@primevue/themes/aura';
 import DataTable from 'primevue/datatable'
 import Column from 'primevue/column'
 import InputText from 'primevue/inputtext'
 import Dropdown from 'primevue/dropdown'
-
+import Button from 'primevue/button'
 import App from './App.vue'
 import router from './ui/router'
 
@@ -16,17 +16,22 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+
 app.use(PrimeVue, {
     theme: {
-        preset: Lara,
-        appearance: 'light'
-    },
-    ripple: true
-})
+        preset: Aura,
+        options: {
+            prefix: 'p',
+            darkModeSelector: false || 'none',
+            cssLayer: false
+        }
+    }
+});
 
 app.component('DataTable', DataTable)
 app.component('Column', Column)
 app.component('InputText', InputText)
 app.component('Dropdown', Dropdown)
+app.component('Button', Button)
 
 app.mount('#app')
--- src/ui/views/HomeView.vue
+++ src/ui/views/HomeView.vue
@@ -1,8 +1,35 @@
 <script setup>
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+
+const navigate = (path) => {
+  router.push(path);
+};
 </script>
 
 <template>
   <main>
-    
+    <div class="flex flex-col items-center gap-6 p-42">
+      <img src="/ST_logo_2020.svg.png" alt="ST Logo" class="w-64 h-auto" />
+
+      <div class="flex flex-col gap-4" style="display: flex; flex-direction: column; gap: 1rem;">
+        <Button @click="navigate('/boards')" severity="success" raised class="w-48"
+          style="width: 350px; font-size: 20px; font-weight: bold;">
+          Boards
+        </Button>
+
+        <Button @click="navigate('/auto-shields')" severity="success" raised class="w-48"
+          style="width: 350px; font-size: 20px; font-weight: bold;">
+          Auto Shields
+        </Button>
+
+        <Button @click="navigate('/setups')" severity="success" raised class="w-48"
+          style="width: 350px; font-size: 20px; font-weight: bold;">
+          Setups
+        </Button>
+      </div>
+    </div>
+
   </main>
 </template>
